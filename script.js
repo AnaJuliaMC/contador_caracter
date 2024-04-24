812c36d6e3d36cce4542ec311268109180b7621c
Ana Julia Menegasso <130059630+AnaJuliaMC@users.noreply.github.com>
2024-04-24 11:44:30 -0300

    js

--- script.js
+++ script.js
@@ -9,19 +9,39 @@ inputTexto.addEventListener("input", handleImputTexto)
 btnContar.addEventListener("click", handleBtnContar)
 
 function handleImputTexto(evento){
-   let tamanho = inputTexto.value.length
-   console.log(tamanho) 
 
+
+   let tamanho = inputTexto.value.length
+   if (modo === "caracteres"){
+    if (tamanho == 1){
+        divContador.innerHTML = tamanho + " caractere"
+       }else{
+        divContador.innerHTML = tamanho + " caracteres"}
+
+   }else{
+
+    let qntadepalavras = inputTexto.value.split(" ").length
+    if (qntadepalavras == 1){
+        divContador.innerHTML = qntadepalavras + " palavra"
+       }else{
+        divContador.innerHTML = qntadepalavras + " palavras"}
+     
+ }
 }
+ 
+
 
 function handleBtnContar(evento){
-    console.log("click contar")
     if (modo === "caracteres"){
         modo = "palavras "
+        divContador.innerHTML = "0 palavras"
         btnContar.innerHTML = "Contar Palavras"
+
     } else{
         modo = "caracteres"
+        divContador.innerHTML = "0 caracteres"
         btnContar.innerHTML = "Contar Caracteres"
     }
+
 
 }
